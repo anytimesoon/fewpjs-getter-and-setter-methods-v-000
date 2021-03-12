1538a42e248d626ca9fdfd62082bdc4b486f53eb
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 17:37:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ class Circle {
   }
   
   get diameter() {
-    
+    this.radius * 2;
   }
   
   get circumference() {
-    
+    Math.PI * this.diameter;
   }
   
   get area() {
-    
+    Math.PI * (this.radius * this.radius)
   }
 }
 
