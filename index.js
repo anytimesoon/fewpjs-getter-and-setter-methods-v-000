202aeb94c3d60756eb673528e216a24bea4b026a
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 17:32:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 // Add your Circle class here
 
 class Circle {
-  constructor()
+  constructor(radius){
+    this.radius = radius;
+  }
+  
+  set diameter(diameter) {
+    this._diameter = diameter
+  }  
+  set circumference(circumference) {
+    this._circumference = circumference
+  }  
+  set area(area) {
+    this._area = area
+  }
+  
+  get diameter(diameter) {
+    
+  }
+}
+
+class Bird {
+  constructor(name) {
+    this.name = name;
+  }
+ 
+  set phrase(phrase) {
+    this._phrase = phrase;
+  }
+ 
+  get speak() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
 }
