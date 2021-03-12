b0b349725915523e7e1a7772a04ce3515fe4b5a0
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 18:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Circle {
   }  
   
   set circumference(circumference) {
-    this.radius = circumference / (Math.PI);
+    this.radius = circumference / (Math.PI * 2);
     this._circumference = circumference;
   }  
   
